85fb632a3a8de046d7241b33b8138bb96f789ac9
Mike Clark <mike@clarkware.com>
2011-06-26 08:45:50 -0600

    Depend on irb_plugin

--- lib/view_source/irb_plugin.rb
+++ lib/view_source/irb_plugin.rb
@@ -1,9 +1,7 @@
-module ViewSource
-  class Object
-    def source_for_method(method)
-      location = self.method(method).source_location
-      `mate #{location[0]} -l #{location[1]}` if location
-      location
-    end
+class Object
+  def source_for_method(method)
+    location = self.method(method).source_location
+    `mate #{location[0]} -l #{location[1]}` if location
+    location
   end
 end
